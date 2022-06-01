95f0ad9d44ae802ee7d192178d07a257ad7237b5
Mo-Greene <13blueboy13@daum.net>
2022-06-01 23:35:19 +0900

    homecontroller

--- src/main/java/com/sparta/security0531/controller/HomeController.java
+++ src/main/java/com/sparta/security0531/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     //기존이였으면 static에 index 찾았겠지만 이제는 동적으로 바꿔줬다.
     @GetMapping("/")
-    // 밑에 내용은 왜워야됨 혹은 복붙 하자
+    // 밑에 내용은 왜워야됨 혹은 복붙 하자  이거 토큰을 만들어준거군요? 로그인 후 디테일 impl을 건네준거 토큰씌어서 구웃
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         model.addAttribute("username", userDetails.getUsername());
         // 동적 index 파일로 간다~
